784e387bb03498a08b8d0f9764b9e9f8d2b8ded1
Francesco negretti <francecesco@hotmail.com>
2015-08-09 19:06:06 +0200

    Commit1 OLD
    VECCHIE PROVE SITO

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+      $('.carousel').carousel({
+      interval: 3000,
+      pause: "false"
+      })
+    })
+
+
+function changeText(){
+    window.location.href = "http://localhost/Mywebsite/home.html";
+  }
+
+$(function shift() {
+      $('#nav nav-pills li:eq(1) a').tab('show');
+   });
